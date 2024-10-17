fd71321453ce8ce5a9cec971efac9793ecdeafd8
Game-Killer-s <artiom.bondar.1995@gmail.com>
2024-10-17 18:36:30 +0300

    Fully recreated project

--- Exam_calculator/Functions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-using namespace std;
-
-void mainLogick(string inputData,bool startFromFunction = 0);
-int mainArithmetic(string inputData);
--- Exam_calculator/Source.cpp
+++ Exam_calculator/Source.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include "functions.h"
 
 using namespace std;
 
 int main() {
-	string i1;
-	i1 = "1+2+3+4+5+6+7+8+9+10(1+2+3+4+5(2*5))";//    676
-	cout << i1.length() << endl;
-	cout << i1 << endl;
-	mainLogick(i1);
+	
 }
